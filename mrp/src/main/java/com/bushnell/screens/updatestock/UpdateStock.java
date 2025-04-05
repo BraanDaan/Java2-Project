ad1e51e88f6ef49d666f12d7d3d9c2a78cd97771
Brandon Mitchell <BraanDaan>
2025-04-04 20:07:38 -0700

    category box displays skus, added dynamic disc text

--- mrp/src/main/java/com/bushnell/screens/updatestock/UpdateStock.java
+++ mrp/src/main/java/com/bushnell/screens/updatestock/UpdateStock.java
@@ -4,6 +4,14 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -15,9 +23,45 @@ import com.bushnell.GUI;
 
 public class UpdateStock {
     public static JPanel MakeGUI(JPanel cardPanel) {
+        // Register the JDBC Driver
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Attempt to create a SQL Query to list all strings under the sku column and store them into a dynamic list
+        ArrayList<String> skuTemp = new ArrayList<String>();
+        try
+        (
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\branm\\OneDrive\\College\\Bushnell2024-2025\\Spring\\SFTE212\\project_repos\\brandon\\Java2-Project\\VR-Factory.db");
+            Statement statement = connection.createStatement();
+        )
+        {
+            ResultSet rs = statement.executeQuery("select * from part order by description;");
+            while(rs.next())
+            {
+                skuTemp.add(rs.getString("sku"));
+            }
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace(System.err);
+        }
+
+        // Take the list and convert it to a standard array to use for the Combo Box
+        String[] skuOptions = new String[skuTemp.size()];
+        try {
+            for (int i = 0; i < skuTemp.size(); i++) {
+                skuOptions[i] = skuTemp.get(i);
+            } 
+        } catch (Exception e) {
+        }
+
+        // Initalize panel
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.setBackground(Color.WHITE); // Bushnell Blue
+        panel.setBackground(Color.WHITE);
 
         panel.add(Box.createRigidArea(new Dimension(0,25)));
 
@@ -30,24 +74,62 @@ public class UpdateStock {
         panel.add(titleBox);
         panel.add(Box.createRigidArea(new Dimension(0,25)));
 
-                // create advice type combo box
+        // create sku combo box from the newly declared array
         panel.add(Box.createRigidArea(new Dimension(0,20)));
-        Box adviceCategoryBox = Box.createHorizontalBox();
-        adviceCategoryBox.setAlignmentX(Component.CENTER_ALIGNMENT);
-        adviceCategoryBox.setAlignmentY(Component.TOP_ALIGNMENT);
-        JLabel adviceCategoryLabel = GUI.text("sku", 10, 30, 20, Color.BLACK, "left");
-        adviceCategoryLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        GUI.setDimension(adviceCategoryLabel, 350,30);
-        adviceCategoryBox.add(adviceCategoryLabel);
-        // adviceCategoryBox.add(Box.createRigidArea(new Dimension(20,0)));
-        String[] adviceCategories = {"Anger", "Stress", "Relationship", "Depression"};
-        JComboBox<String> adviceCategory = new JComboBox<>(adviceCategories); 
-        GUI.setDimension(adviceCategory, 150, 30);            
-        adviceCategory.setFont(new Font("Sans-Serif", Font.BOLD, 20));
-        // adviceCategory.setAlignmentX(Component.CENTER_ALIGNMENT);     
-        adviceCategoryBox.add(adviceCategory);
-        panel.add(adviceCategoryBox);
+        Box skuOptionBox = Box.createHorizontalBox();
+        skuOptionBox.setAlignmentX(Component.CENTER_ALIGNMENT);
+        skuOptionBox.setAlignmentY(Component.TOP_ALIGNMENT);
+        JLabel skuOptionLabel = GUI.text("sku", 10, 30, 20, Color.BLACK, "left");
+        skuOptionLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        GUI.setDimension(skuOptionLabel, 350,30);
+        skuOptionBox.add(skuOptionLabel);
+        // skuOptionBox.add(Box.createRigidArea(new Dimension(20,0)));
+        JComboBox<String> skuOption = new JComboBox<>(skuOptions); 
+        GUI.setDimension(skuOption, 150, 30);            
+        skuOption.setFont(new Font("Sans-Serif", Font.BOLD, 20));
+        // skuOption.setAlignmentX(Component.CENTER_ALIGNMENT);     
+        skuOptionBox.add(skuOption);
+        panel.add(skuOptionBox);
+        
         panel.add(Box.createRigidArea(new Dimension(0,20)));
+        Box descriptionBox = Box.createHorizontalBox();
+        descriptionBox.setAlignmentX(Component.CENTER_ALIGNMENT);
+        descriptionBox.setAlignmentY(Component.TOP_ALIGNMENT);
+        JLabel discriptionLabel = GUI.text("description", 10, 30, 20, Color.BLACK, "left");
+        discriptionLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        GUI.setDimension(discriptionLabel, 350,30);
+        descriptionBox.add(discriptionLabel);
+
+        JLabel partDescription = new JLabel("");
+        partDescription.setFont(new Font("Sans-Serif", Font.BOLD, 20));
+        partDescription.setAlignmentX(Component.LEFT_ALIGNMENT);
+        GUI.setDimension(partDescription, 350, 30);
+        descriptionBox.add(partDescription);
+        panel.add(descriptionBox);
+        
+        skuOption.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try
+                (
+                    Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\branm\\OneDrive\\College\\Bushnell2024-2025\\Spring\\SFTE212\\project_repos\\brandon\\Java2-Project\\VR-Factory.db");
+                    Statement statement = connection.createStatement();
+                )
+                {
+                    String query = ("SELECT * FROM part WHERE sku='" + skuOption.getItemAt(skuOption.getSelectedIndex()) + "';");
+                    ResultSet rs = statement.executeQuery(query);
+                    while(rs.next())
+                    {
+                        partDescription.setText(rs.getString("description"));
+                    }
+                }
+                catch(SQLException f)
+                {
+                    f.printStackTrace(System.err);
+                }
+            }
+        });
+
         return panel;
     }
 }
